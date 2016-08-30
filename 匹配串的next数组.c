d25c9434d09ae73d84abacfb63fef4d41288700a
SheenChow <15538398912@163.com>
2016-08-30 17:38:36 +0800

    KMP算法实现之next数组
    计算当前匹配串中的next数组

---
+++ 匹配串的next数组.c
@@ -0,0 +1,35 @@
+/*
+*KMP模式匹配算法的实现
+*通过计算返回子串T的next数组
+*/
+void get_next(String T, int* next){
+
+	int i = 1;
+
+	int j = 0; 
+
+	next[1] = 0;
+
+	//T[0]中存储的是字符串T的长度
+	while(i < T[0]){ 
+
+		/*
+		*T[i]代表后缀中的每一项
+		*T[j]代表前缀中的每一项
+		*/
+		if(j == 0 || T[i] == T[j]){
+
+			++i;
+
+			++j;
+		
+		    next[i] = j;
+
+		} else {
+			
+            /*若字符不相同，则j至回溯*/
+			j = next[j];
+
+		}
+	}
+}
